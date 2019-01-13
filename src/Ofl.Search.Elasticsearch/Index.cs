0ef57dac8cba9214aae6c4742a0537573b2a36ec
casperOne <casperOne@caspershouse.com>
2019-01-13 18:33:33 -0500

    Updated references, implemented ExistsAsync.

--- src/Ofl.Search.Elasticsearch/Index.cs
+++ src/Ofl.Search.Elasticsearch/Index.cs
@@ -13,61 +13,44 @@ namespace Ofl.Search.Elasticsearch
         protected Index(string name, IElasticClient elasticClient) : base(name)
         {
             // Validate parameters.
-            _elasticClient = elasticClient
+            ElasticClient = elasticClient
                 ?? throw new ArgumentNullException(nameof(elasticClient));
-
-            // Assign values.
-            _indexName = IndexName.From<T>();
         }
 
         #endregion
 
         #region Instance, read-only state.
 
-        private readonly IElasticClient _elasticClient;
-
-        private readonly IndexName _indexName;
-
-        #endregion
-
-        #region To-be overridden
-
-        protected abstract Task PopulateAsync(CancellationToken cancellationToken);
-
-        #endregion
-
-        #region Helpers.
-
-        private async Task<IDeleteIndexResponse> DestroyImplementationAsync(CancellationToken cancellationToken)
-        {
-            // The request.
-            IDeleteIndexRequest request = new DeleteIndexDescriptor(Indices.Index(_indexName));
-
-            // Delete and return the response.
-            return await _elasticClient.DeleteIndexAsync(request, cancellationToken).ConfigureAwait(false);
-        }
+        protected readonly IElasticClient ElasticClient;
 
         #endregion
 
         #region Overrides of Index
 
-        public override async Task RegenerateAsync(CancellationToken cancellationToken)
+        public override async Task<bool> ExistsAsync(CancellationToken cancellationToken)
         {
-            // Destroy.
-            await DestroyImplementationAsync(cancellationToken).ConfigureAwait(false);
+            // Create the request.
+            IIndexExistsRequest request = new IndexExistsDescriptor(Indices.Index<T>());
 
-            // Create.
-            await CreateAsync(cancellationToken).ConfigureAwait(false);
+            // Get the response.
+            IExistsResponse response = await ElasticClient.IndexExistsAsync(request, cancellationToken)
+                .ConfigureAwait(false);
 
-            // Populate.
-            await PopulateAsync(cancellationToken);
+            // Throw if invalid.
+            response.ThrowIfError();
+
+            // Return the response.
+            return response.Exists;
         }
 
         public override async Task DestroyAsync(CancellationToken cancellationToken)
         {
-            // Call the implementation.
-            IDeleteIndexResponse response = await DestroyImplementationAsync(cancellationToken).
-                ConfigureAwait(false);
+            // The request.
+            IDeleteIndexRequest request = new DeleteIndexDescriptor(Indices.Index<T>());
+
+            // Delete and return the response.
+            IDeleteIndexResponse response = await ElasticClient
+                .DeleteIndexAsync(request, cancellationToken).ConfigureAwait(false);
 
             // If it failed, throw an exception.
             response.ThrowIfError();
@@ -76,24 +59,20 @@ namespace Ofl.Search.Elasticsearch
         public override Task<IIndexWriteOperations<T>> GetWriteOperationsAsync(CancellationToken cancellationToken)
         {
             // Create the write operations.
-            IIndexWriteOperations<T> ops = new IndexWriteOperations<T>(_elasticClient, this);
+            IIndexWriteOperations<T> ops = new IndexWriteOperations<T>(ElasticClient, this);
 
             // Return.
             return Task.FromResult(ops);
         }
-
-        #region Overrides of Index
 
         public override Task<IIndexReadOperations<T>> GetReadOperationsAsync(CancellationToken cancellationToken)
         {
             // Create the read operations.
-            IIndexReadOperations<T> ops = new IndexReadOperations<T>(_elasticClient, this);
+            IIndexReadOperations<T> ops = new IndexReadOperations<T>(ElasticClient, this);
 
             // Return.
             return Task.FromResult(ops);
         }
-
-        #endregion
 
         #endregion
     }
